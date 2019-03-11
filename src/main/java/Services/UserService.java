433de68c55d09e6a0e3ee0aeae5d55f6280eea28
AxWeLL <axwell33@gmail.com>
2019-03-12 05:05:47 +0800

    version 1.1 class UserService added to Services

--- src/main/java/Services/UserService.java
+++ src/main/java/Services/UserService.java
@@ -3,8 +3,10 @@ package Services;
 import DAO.UserDAO;
 import Entity.Aim;
 import Entity.User;
+import Utilities.Util;
 
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserService implements UserDAO {
@@ -13,22 +15,130 @@ public class UserService implements UserDAO {
     }
 
     public List<User> getAll() throws SQLException {
-        return null;
+        String query = "select * from users";
+
+        Util.connectToDB();
+
+        Connection connection = Util.connection;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        List<User> userList = new ArrayList<User>();
+
+        while (resultSet.next()) {
+
+            User user = new User(
+                    resultSet.getInt(1),
+                    resultSet.getString(2),
+                    resultSet.getInt(3),
+                    resultSet.getInt(4));
+
+            userList.add(user);
+        }
+        return userList;
     }
 
     public List<Aim> getAllAims(User user) throws SQLException {
-        return null;
+        String query = "select * from aims";
+
+        Util.connectToDB();
+
+        Connection connection = Util.connection;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        List<Aim> aimsList = new ArrayList<Aim>();
+
+        while (resultSet.next()) {
+
+            Aim aim = new Aim(
+                    resultSet.getInt(1),
+                    resultSet.getString(2),
+                    resultSet.getDate(3),
+                    resultSet.getBoolean(4));
+
+            aimsList.add(aim);
+        }
+        return aimsList;
     }
 
     public User getByID(int id) {
-        return null;
+        String query = "SELECT * FROM users WHERE id=" + id;
+
+        Util.connectToDB();
+
+        Connection connection = Util.connection;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        User user = null;
+
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+            user = new User(
+                    resultSet.getInt(1),
+                    resultSet.getString(2),
+                    resultSet.getInt(3),
+                    resultSet.getInt(4)
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     public void update(User user) {
 
+        Util.connectToDB();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("UPDATE users SET id=\'");
+        stringBuilder.append(user.getId());
+        stringBuilder.append("\',login=\'");
+        stringBuilder.append(user.getLogin());
+        stringBuilder.append("\',password=\'");
+        stringBuilder.append(user.getPassword());
+        stringBuilder.append("\',role=\'");
+        stringBuilder.append(user.getRole());
+        stringBuilder.append("\'WHERE id=");
+        stringBuilder.append(user.getId());
+        System.out.println(stringBuilder.toString());
+        try {
+            Statement statement = Util.connection.createStatement();
+            statement.executeUpdate(stringBuilder.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Util.disconnectFromDB();
     }
 
     public void remove(User user) {
 
+        Util.connectToDB();
+        PreparedStatement preparedStatement = null;
+        String deleteData = "DELETE FROM users WHERE id=" + user.getId();
+
+        try {
+            preparedStatement = Util.connection.prepareStatement(deleteData);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Util.disconnectFromDB();
     }
 }
